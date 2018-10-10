2a7da052648b523b60d7a2ad84cb2601e7bb2532
qWici <ivk@elementalsweb.com>
2018-10-10 19:18:53 +0300

    Add :: favicons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,6 +27,14 @@ $polyfills = [
   <title>{{ config('app.name') }}</title>
 
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicon/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('/favicon/site.webmanifest') }}">
+  <link rel="mask-icon" href="{{ asset('/favicon/safari-pinned-tab.svg') }}" color="#0f0f0f">
+  <meta name="msapplication-TileColor" content="#423a6f">
+  <meta name="msapplication-TileImage" content="{{ asset('/favicon/mstile-144x144.png') }}">
+  <meta name="theme-color" content="#423a6f">
 </head>
 <body>
   <div id="app"></div>
