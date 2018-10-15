98ce835c8663e221ae85bb52614d059ae03f6621
Vania_Kucher <dev.kucher@gmail.com>
2018-10-16 01:03:29 +0300

    Update FA packages

--- resources/js/plugins/fontawesome.js
+++ resources/js/plugins/fontawesome.js
@@ -4,18 +4,20 @@ import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
 import {
   faNewspaper
-} from '@fortawesome/fontawesome-free-regular/shakable.es'
+} from '@fortawesome/free-regular-svg-icons'
 
 import {
   faUser, faLock, faSignOutAlt, faCog, faCheck, faEnvelope, faUserLock, faTv, faMicrophone
-} from '@fortawesome/fontawesome-free-solid/shakable.es'
+} from '@fortawesome/fontawesome-free-solid'
 
 import {
   faGithub, faYoutube,
-} from '@fortawesome/fontawesome-free-brands/shakable.es'
+} from '@fortawesome/free-brands-svg-icons'
 
 fontawesome.library.add(
   faUser, faLock, faSignOutAlt, faCog, faGithub, faCheck, faEnvelope, faUserLock, faNewspaper, faTv, faYoutube, faNewspaper, faMicrophone
 )
+
+dom.watch()
 
 Vue.component('fa', FontAwesomeIcon)
