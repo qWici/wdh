67d27a2bc460beee3dafe0b4633f0d3d932026b8
qWici <ivk@elementalsweb.com>
2018-11-02 11:41:21 +0200

    Refactor console commands

--- app/Console/Commands/Articles/CheckNewArticle.php
+++ app/Console/Commands/Articles/CheckNewArticle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Articles;
 
 use App\Models\Article;
 use App\Models\Author;
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use App\Console\Commands\CheckStreamsOnline;
+use App\Console\Commands\Streams\CheckStreamsOnline;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
