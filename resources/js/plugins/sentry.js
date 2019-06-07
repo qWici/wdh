d2f8b80b8cd922f7113e47b6e26700b35e66af9e
Ivan Kucher <dev.kucher@gmail.com>
2019-06-07 16:49:49 +0300

    Setup sentry & logrocket

--- resources/js/plugins/sentry.js
+++ resources/js/plugins/sentry.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+import LogRocket from 'logrocket'
 import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+LogRocket.getSessionURL(sessionURL => {
+  Sentry.configureScope(scope => {
+    scope.setExtra('sessionURL', sessionURL)
+  })
+})
 
 Sentry.init({
-  dsn: 'https://7c073845327440288818f593adbff883@sentry.io/1356292',
-  integrations: [new Sentry.Integrations.Vue({
-    Vue,
-    attachProps: true
-  })]
+  dsn: 'https://6f65ee400b294a13ab0b4c8ccbb92e3b@sentry.io/1412774',
+  integrations: [
+    new Integrations.Vue({ Vue, attachProps: true })
+  ]
 })
